e24de8168417f876d1b159637fd1ea0bddcaaac6
Muhammad <muhammadhullah23@gmail.com>
2021-03-05 22:56:08 -0500

    commit tree 3475ac71e1e730acdbaa0bd1fe053449506b3e82

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(integers)
+    integers.sort
+end
 
+def sort_array_desc(integers)
+    integers.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(strings)
+    strings.sort do |a, b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements(names)
+    names = names[0], names[2], names[1]
+end
+
+def reverse_array(integers)
+    integers.reverse
+end
+
+def kesha_maker(array)
+    array.each do |third|
+        third[2] = "$"
+    end
+end
+
+def find_a(fruit)
+    fruit.select{|letter| letter[0] == "a"}
+end
+
+def sum_array(numbers)
+    sum = 0
+    numbers.each do |num|
+        sum += num
+    end
+    sum
+end
+
+def add_s(things)
+    things.collect do |item|
+        if things[1] == item
+            item
+        else
+            item + "s"
+        end
+    end
+end
